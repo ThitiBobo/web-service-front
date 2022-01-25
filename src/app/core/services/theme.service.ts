3fe4f06612bfc8ff8ac13a1931011fd33e958253
Thibaut Delplanque <t.delplanque92@gmail.com>
2022-01-25 20:18:05 +0100

    DEV : theme service work with local storage

--- src/app/core/services/theme.service.ts
+++ src/app/core/services/theme.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 
+const LOCAL_STORAGE_KEY = "dark-mode"
+
 @Injectable({
   providedIn: 'root'
 })
@@ -7,7 +9,16 @@ export class ThemeService {
 
   private darkMode: boolean = false;
 
-  constructor() { }
+  constructor() {
+    console.log("contructor")
+    if (!(localStorage.getItem(LOCAL_STORAGE_KEY) === null)) {
+      console.log("localstrorage == true")
+      let value = localStorage.getItem(LOCAL_STORAGE_KEY)
+      if (typeof value === "string") {
+        this.darkMode = JSON.parse(value) === true
+      }
+    }
+  }
 
   isDarkMode(): boolean{
     return this.darkMode;
@@ -15,5 +26,6 @@ export class ThemeService {
 
   switchTheme(){
     this.darkMode = !this.darkMode;
+    localStorage.setItem(LOCAL_STORAGE_KEY, String(this.darkMode));
   }
 }
